0d77599be40fa8873f0ff40aca71123f1adce8e7
Distil <Distil62@github.com>
2018-11-14 20:55:35 +0100

    4

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -1,14 +1,15 @@
 import 'package:angular/angular.dart';
-import 'package:angular_forms/angular_forms.dart';
 
 import './src/Hero.dart';
 import './src/mock_heroes.dart';
+
+import './hero_component.dart';
 
 @Component(
   selector: 'my-app',
   templateUrl: 'app_component.html',
   styleUrls: ['app_component.css'],
-  directives: [coreDirectives, formDirectives]
+  directives: [coreDirectives, HeroComponent]
 )
 
 class AppComponent {
