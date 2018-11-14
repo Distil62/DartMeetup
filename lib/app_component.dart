79171e916cc3e894e191534df4b0cb0f5939cfbe
Distil <Distil62@github.com>
2018-11-14 20:11:48 +0100

    2

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -1,11 +1,24 @@
 import 'package:angular/angular.dart';
+import 'package:angular_forms/angular_forms.dart';
+
+import 'Hero.dart';
 
 @Component(
   selector: 'my-app',
-  template: '<h1>Hello {{title}}</h1>',
+  template: '''
+    <h1>{{ title }}</h1>
+    <h2>{{ hero.name }}</h2>
+    <div>
+      <label>id :</label> {{hero.id}}
+    </div>
+    <div>
+    <input [(ngModel)]="hero.name" placeholder="name" >
+    </div>
+    ''',
+    directives: [formDirectives]
 )
 
 class AppComponent {
   final title = "Tour of Heroes";
-  String hero = "Windstorm";
+  Hero hero = Hero(1, "Windsom");
 }
